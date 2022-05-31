0da9163a627759326e77879bab174fed79eb6427
sincerely-chiche <immicks@gmail.com>
2022-05-31 16:42:51 +0100

    added  carousel from daisy

--- src/pages/dashboard/IndexPage.vue
+++ src/pages/dashboard/IndexPage.vue
@@ -1,11 +1,40 @@
 <template>
   <q-page padding>
-    <header class="bg-white shadow">
-      <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-        <h1 class="text-3xl font-bold text-gray-900">Dashboard</h1>
+    <header class="">
+      <div class="d-carousel shadow w-full rounded-box">
+        <div id="item1" class="d-carousel-item w-full">
+          <img
+            src="https://api.lorem.space/image/car?w=800&h=200&hash=8B7BCDC2"
+            class="w-full"
+          />
+        </div>
+        <div id="item2" class="d-carousel-item w-full">
+          <img
+            src="https://api.lorem.space/image/car?w=800&h=200&hash=500B67FB"
+            class="w-full"
+          />
+        </div>
+        <div id="item3" class="d-carousel-item w-full">
+          <img
+            src="https://api.lorem.space/image/car?w=800&h=200&hash=A89D0DE6"
+            class="w-full"
+          />
+        </div>
+        <div id="item4" class="d-carousel-item w-full">
+          <img
+            src="https://api.lorem.space/image/car?w=800&h=200&hash=225E6693"
+            class="w-full"
+          />
+        </div>
+      </div>
+      <div class="flex justify-center w-full py-2 gap-2">
+        <a href="#item1" class="d-btn d-btn-xs">1</a>
+        <a href="#item2" class="d-btn d-btn-xs">2</a>
+        <a href="#item3" class="d-btn d-btn-xs">3</a>
+        <a href="#item4" class="d-btn d-btn-xs">4</a>
       </div>
     </header>
-    <div class="d-stats shadow mt-10">
+    <div class="d-stats shadow mt-10 w-full">
       <div class="d-stat">
         <div class="d-stat-figure text-primary">
           <svg
@@ -61,120 +90,122 @@
         <div class="d-stat-desc text-secondary">31 tasks remaining</div>
       </div>
     </div>
-    <ul class="p-4 lg:p-8 dark:bg-gray-800 dark:text-gray-100">
-      <li>
-        <article>
-          <a
-            rel="noopener noreferrer"
-            href="#"
-            class="grid p-4 overflow-hidden md:grid-cols-5 rounded-xl lg:p-6 xl:grid-cols-12 hover:dark:bg-gray-900"
-          >
-            <h3
-              class="mb-1 ml-8 font-semibold md:col-start-2 md:col-span-4 md:ml-0 xl:col-start-3 xl:col-span-9"
+    <div class="w-full">
+      <ul class="p-4 lg:p-8 dark:bg-gray-800 dark:text-gray-100 w-full">
+        <li>
+          <article>
+            <a
+              rel="noopener noreferrer"
+              href="#"
+              class="grid p-4 overflow-hidden md:grid-cols-5 rounded-xl lg:p-6 xl:grid-cols-12 hover:dark:bg-gray-900"
             >
-              Earum at ipsa aliquid quis, exercitationem est.
-            </h3>
-            <time
-              datetime=""
-              class="row-start-1 mb-1 md:col-start-1 xl:col-span-2 dark:text-gray-400"
-              >Oct 13, 2020</time
+              <h3
+                class="mb-1 ml-8 font-semibold md:col-start-2 md:col-span-4 md:ml-0 xl:col-start-3 xl:col-span-9"
+              >
+                Earum at ipsa aliquid quis, exercitationem est.
+              </h3>
+              <time
+                datetime=""
+                class="row-start-1 mb-1 md:col-start-1 xl:col-span-2 dark:text-gray-400"
+                >Oct 13, 2020</time
+              >
+              <p
+                class="ml-8 md:col-start-2 md:col-span-4 xl:col-start-3 xl:col-span-9 md:ml-0 dark:text-gray-300"
+              >
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Similique saepe
+                exercitationem numquam, labore necessitatibus deleniti quasi. Illo porro
+                nihil necessitatibus debitis delectus aperiam, fuga impedit assumenda
+                odit, velit eveniet est.
+              </p>
+            </a>
+          </article>
+        </li>
+        <li>
+          <article>
+            <a
+              rel="noopener noreferrer"
+              href="#"
+              class="grid p-4 overflow-hidden md:grid-cols-5 rounded-xl lg:p-6 xl:grid-cols-12 hover:dark:bg-gray-900"
             >
-            <p
-              class="ml-8 md:col-start-2 md:col-span-4 xl:col-start-3 xl:col-span-9 md:ml-0 dark:text-gray-300"
+              <h3
+                class="mb-1 ml-8 font-semibold md:col-start-2 md:col-span-4 md:ml-0 xl:col-start-3 xl:col-span-9"
+              >
+                Earum at ipsa aliquid quis, exercitationem est.
+              </h3>
+              <time
+                datetime=""
+                class="row-start-1 mb-1 md:col-start-1 xl:col-span-2 dark:text-gray-400"
+                >Oct 13, 2020</time
+              >
+              <p
+                class="ml-8 md:col-start-2 md:col-span-4 xl:col-start-3 xl:col-span-9 md:ml-0 dark:text-gray-300"
+              >
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Similique saepe
+                exercitationem numquam, labore necessitatibus deleniti quasi. Illo porro
+                nihil necessitatibus debitis delectus aperiam, fuga impedit assumenda
+                odit, velit eveniet est.
+              </p>
+            </a>
+          </article>
+        </li>
+        <li>
+          <article>
+            <a
+              rel="noopener noreferrer"
+              href="#"
+              class="grid p-4 overflow-hidden md:grid-cols-5 rounded-xl lg:p-6 xl:grid-cols-12 hover:dark:bg-gray-900"
             >
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Similique saepe
-              exercitationem numquam, labore necessitatibus deleniti quasi. Illo porro
-              nihil necessitatibus debitis delectus aperiam, fuga impedit assumenda odit,
-              velit eveniet est.
-            </p>
-          </a>
-        </article>
-      </li>
-      <li>
-        <article>
-          <a
-            rel="noopener noreferrer"
-            href="#"
-            class="grid p-4 overflow-hidden md:grid-cols-5 rounded-xl lg:p-6 xl:grid-cols-12 hover:dark:bg-gray-900"
-          >
-            <h3
-              class="mb-1 ml-8 font-semibold md:col-start-2 md:col-span-4 md:ml-0 xl:col-start-3 xl:col-span-9"
+              <h3
+                class="mb-1 ml-8 font-semibold md:col-start-2 md:col-span-4 md:ml-0 xl:col-start-3 xl:col-span-9"
+              >
+                Earum at ipsa aliquid quis, exercitationem est.
+              </h3>
+              <time
+                datetime=""
+                class="row-start-1 mb-1 md:col-start-1 xl:col-span-2 dark:text-gray-400"
+                >Oct 13, 2020</time
+              >
+              <p
+                class="ml-8 md:col-start-2 md:col-span-4 xl:col-start-3 xl:col-span-9 md:ml-0 dark:text-gray-300"
+              >
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Similique saepe
+                exercitationem numquam, labore necessitatibus deleniti quasi. Illo porro
+                nihil necessitatibus debitis delectus aperiam, fuga impedit assumenda
+                odit, velit eveniet est.
+              </p>
+            </a>
+          </article>
+        </li>
+        <li>
+          <article>
+            <a
+              rel="noopener noreferrer"
+              href="#"
+              class="grid p-4 overflow-hidden md:grid-cols-5 rounded-xl lg:p-6 xl:grid-cols-12 hover:dark:bg-gray-900"
             >
-              Earum at ipsa aliquid quis, exercitationem est.
-            </h3>
-            <time
-              datetime=""
-              class="row-start-1 mb-1 md:col-start-1 xl:col-span-2 dark:text-gray-400"
-              >Oct 13, 2020</time
-            >
-            <p
-              class="ml-8 md:col-start-2 md:col-span-4 xl:col-start-3 xl:col-span-9 md:ml-0 dark:text-gray-300"
-            >
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Similique saepe
-              exercitationem numquam, labore necessitatibus deleniti quasi. Illo porro
-              nihil necessitatibus debitis delectus aperiam, fuga impedit assumenda odit,
-              velit eveniet est.
-            </p>
-          </a>
-        </article>
-      </li>
-      <li>
-        <article>
-          <a
-            rel="noopener noreferrer"
-            href="#"
-            class="grid p-4 overflow-hidden md:grid-cols-5 rounded-xl lg:p-6 xl:grid-cols-12 hover:dark:bg-gray-900"
-          >
-            <h3
-              class="mb-1 ml-8 font-semibold md:col-start-2 md:col-span-4 md:ml-0 xl:col-start-3 xl:col-span-9"
-            >
-              Earum at ipsa aliquid quis, exercitationem est.
-            </h3>
-            <time
-              datetime=""
-              class="row-start-1 mb-1 md:col-start-1 xl:col-span-2 dark:text-gray-400"
-              >Oct 13, 2020</time
-            >
-            <p
-              class="ml-8 md:col-start-2 md:col-span-4 xl:col-start-3 xl:col-span-9 md:ml-0 dark:text-gray-300"
-            >
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Similique saepe
-              exercitationem numquam, labore necessitatibus deleniti quasi. Illo porro
-              nihil necessitatibus debitis delectus aperiam, fuga impedit assumenda odit,
-              velit eveniet est.
-            </p>
-          </a>
-        </article>
-      </li>
-      <li>
-        <article>
-          <a
-            rel="noopener noreferrer"
-            href="#"
-            class="grid p-4 overflow-hidden md:grid-cols-5 rounded-xl lg:p-6 xl:grid-cols-12 hover:dark:bg-gray-900"
-          >
-            <h3
-              class="mb-1 ml-8 font-semibold md:col-start-2 md:col-span-4 md:ml-0 xl:col-start-3 xl:col-span-9"
-            >
-              Earum at ipsa aliquid quis, exercitationem est.
-            </h3>
-            <time
-              datetime=""
-              class="row-start-1 mb-1 md:col-start-1 xl:col-span-2 dark:text-gray-400"
-              >Oct 13, 2020</time
-            >
-            <p
-              class="ml-8 md:col-start-2 md:col-span-4 xl:col-start-3 xl:col-span-9 md:ml-0 dark:text-gray-300"
-            >
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Similique saepe
-              exercitationem numquam, labore necessitatibus deleniti quasi. Illo porro
-              nihil necessitatibus debitis delectus aperiam, fuga impedit assumenda odit,
-              velit eveniet est.
-            </p>
-          </a>
-        </article>
-      </li>
-    </ul>
+              <h3
+                class="mb-1 ml-8 font-semibold md:col-start-2 md:col-span-4 md:ml-0 xl:col-start-3 xl:col-span-9"
+              >
+                Earum at ipsa aliquid quis, exercitationem est.
+              </h3>
+              <time
+                datetime=""
+                class="row-start-1 mb-1 md:col-start-1 xl:col-span-2 dark:text-gray-400"
+                >Oct 13, 2020</time
+              >
+              <p
+                class="ml-8 md:col-start-2 md:col-span-4 xl:col-start-3 xl:col-span-9 md:ml-0 dark:text-gray-300"
+              >
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Similique saepe
+                exercitationem numquam, labore necessitatibus deleniti quasi. Illo porro
+                nihil necessitatibus debitis delectus aperiam, fuga impedit assumenda
+                odit, velit eveniet est.
+              </p>
+            </a>
+          </article>
+        </li>
+      </ul>
+    </div>
   </q-page>
 </template>
 
